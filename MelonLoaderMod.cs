c444d2a2b7e2b0c35dd74ce30b78c8164c927583
DarkSwitchPro <59634615+DarkSwitchPro@users.noreply.github.com>
2021-12-11 18:57:46 -0500

    BW-VignetteRemover

---
+++ MelonLoaderMod.cs
@@ -0,0 +1,24 @@
+﻿using MelonLoader;
+using UnityEngine;
+using ModThatIsNotMod.BoneMenu;
+
+namespace VignetteRemover
+{
+    public static class BuildInfo
+    {
+        public const string Name = "Vignette Remover";
+        public const string Author = "DarkSwitchPro";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+    }
+
+    public class VignetteRemover : MelonMod 
+    {
+        public override void OnSceneWasInitialized(int buildindex, string sceneName)
+        {
+            GameObject vignette = GameObject.Find("Vignetter");
+            vignette.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(VignetteRemover.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(VignetteRemover.BuildInfo.Company)]
+[assembly: AssemblyProduct(VignetteRemover.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + VignetteRemover.BuildInfo.Author)]
+[assembly: AssemblyTrademark(VignetteRemover.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(VignetteRemover.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(VignetteRemover.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(VignetteRemover.VignetteRemover), VignetteRemover.BuildInfo.Name, VignetteRemover.BuildInfo.Version, VignetteRemover.BuildInfo.Author, VignetteRemover.BuildInfo.DownloadLink)]
+[assembly: MelonGame(null, null)]
